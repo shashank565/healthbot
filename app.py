3e6335d94c9d8ee8196fb9cdde4f2e2567912b8c
shashank565 <63156571+shashank565@users.noreply.github.com>
2020-09-04 14:49:45 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+import os
+
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+filenumber=int(os.listdir('recorded_responses')[-1])
+filenumber=filenumber+1
+file= open('recorded_responses/'+str(filenumber),"w+")
+file.write('bot: Hello im a HEALTHBOT\n')
+file.close()
+
+app = Flask(__name__)
+
+
+english_bot = ChatBot('Bot',
+             storage_adapter='chatterbot.storage.SQLStorageAdapter',
+             logic_adapters=[
+   {
+       'import_path': 'chatterbot.logic.BestMatch'
+   },
+   
+],
+trainer='chatterbot.trainers.ListTrainer')
+english_bot.set_trainer(ListTrainer)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    response = str(english_bot.get_response(userText))
+
+    appendfile=os.listdir('recorded_responses')[-1]
+    appendfile= open('recorded_responses/'+str(filenumber),"a")
+    appendfile.write('user : '+userText+'\n')
+    appendfile.write('bot : '+response+'\n')
+    appendfile.close()
+    return response
+
+
+if __name__ == "__main__":
+    app.run(port=1010,debug=True)
